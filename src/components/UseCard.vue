59ff91d9360d56a66959f876c953f0254742f10a
andreodev <developerandreo@gmail.com>
2025-04-17 19:07:59 -0300

    seccond-commit

--- src/components/UseCard.vue
+++ src/components/UseCard.vue
@@ -1,24 +1,26 @@
 <template>
-  <div 
-    class="p-4 rounded-sm     shadow-md bg-gray-200 mt-6 flex justify-between items-center 
-    hover:scale-105 transition-transform duration-300 ease-in-out" 
+  <div
+    class="p-4 rounded-sm shadow-md mt-6 flex justify-between items-center 
+    transition-all duration-300 ease-in-out" 
+    :style="{ backgroundColor: done ? '#48bb78' : '#edf2f7' }"
   >
-    <p 
-      class="text-lg text-black font-normal" 
-      :class="{ 'line-through ': done }"
+    <p
+      class="text-lg text-black font-normal transition-all duration-300 ease-in-out"
+      :style="{ textDecoration: done ? 'line-through' : 'none', opacity: done ? 0.5 : 1 }"
     >
       {{ task }}
-    </p> <!-- Exibe a tarefa individual -->
+    </p>
+    <!-- Exibe a tarefa individual -->
     <div class="flex gap-2">
       <!-- Botão confirmar -->
-      <button 
+      <button
         class="bg-green-600 text-white px-2 py-1 rounded hover:bg-green-700 hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer"
         @click="markAsDone"
       >
         ✅
       </button>
       <!-- Botão excluir -->
-      <button 
+      <button
         class="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700 cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out"
         @click="deleteTask"
       >
@@ -30,25 +32,25 @@
 
 <script lang="ts" setup>
 defineOptions({
-  name: 'UseCard'
-})
+  name: "UseCard",
+});
 
 const props = defineProps<{
-  task: string // Espera uma tarefa individual (string)
-  index: number
-  done: boolean
-}>()
+  task: string; // Espera uma tarefa individual (string)
+  index: number;
+  done: boolean;
+}>();
 
 const emit = defineEmits<{
-  (e: 'remove', index: number): void
-  (e: "done", index: number): void
-}>()
+  (e: "remove", index: number): void;
+  (e: "done", index: number): void;
+}>();
 
 function markAsDone() {
-  emit('done', props.index)
+  emit("done", props.index);
 }
 
 function deleteTask() {
-  emit('remove', props.index)
+  emit("remove", props.index);
 }
 </script>
